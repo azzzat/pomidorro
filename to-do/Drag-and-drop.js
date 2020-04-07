faf2bc12643a657ad725f3c733b1267bbb40ef74
azzzat <arbuz4az@yandex.ru>
2020-04-07 21:35:14 +0300

    проверка названий файлов

--- to-do/Drag-and-drop.js
+++ to-do/Drag-and-drop.js
@@ -1,4 +1,4 @@
-import { todoList, todoTaskRepeat } from "../todo-border.js";
+import { todoList } from "../todo-border.js";
 
 export let divElem = document.querySelectorAll(".todo-case-bl");
 document.onselectstart = function() {
